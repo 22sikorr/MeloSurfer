b77ce66887f6012481f06299bd539c32d973ef5a
unknown <wojtek.sikor4@gmail.com>
2024-02-14 12:50:31 +0100

    zmiana wygladu strony

---
+++ src/js/header.jsx
@@ -0,0 +1,16 @@
+import { Container } from "semantic-ui-react";
+
+const Header = () => {
+  return (
+    <>
+      <header>
+        <Container className="container">
+        
+        <h1 className="logo-text">MeloSurfer</h1>
+        </Container>
+      </header>
+    </>
+  );
+};
+export default Header;
+{/* <img className="logo" src="src\assets\logo.svg"></img> */}
